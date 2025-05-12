f85b327281c7174514fddd2b6c5d8f5ab1d3be85
Gnuh Le <huyhunglenguyen@gmail.com>
2025-05-12 19:41:41 +0700

    update swagger and DB

--- QuickLearn.BE/Program.cs
+++ QuickLearn.BE/Program.cs
@@ -59,9 +59,10 @@ public class Program
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
-            app.UseSwagger();
-            app.UseSwaggerUI();
+            
         }
+        app.UseSwagger();
+        app.UseSwaggerUI();
 
         app.UseHttpsRedirection();
         app.UseCors("AllowAll");
